66e2d2c981221a38f9566b1f35f47db1a985425a
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-11 00:59:38 +0530

    ADDED: Code to launch bridgelabz site in new tab when logo is clicked

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import "./App.css";
 import logo from "./logo.svg";
 
 class App extends React.Component {
+  url = "https://www.bridgelabz.com/";
   constructor() {
     super();
     this.state = {
       title: "Hello from Bridgelabz",
     };
   }
+  onClick = ($event) => {
+    console.log("save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  };
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
         <img
           src={logo}
+          onClick={this.onClick}
           alt="The Bridgelabz logo : a Bridge to Employment through lab works"
         />
       </div>
